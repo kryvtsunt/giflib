2bd6f154c3bc5aac97af7afcc86a9106fd1860ef
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-08-29 14:02:44 -0400

    implemented dynamically loaded data

---
+++ src/main/java/com/tk/controller/GifController.java
@@ -0,0 +1,34 @@
+package com.tk.controller;
+
+
+import com.tk.data.GifRepository;
+import com.tk.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
+    @RequestMapping("/")
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs",allGifs);
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif",gif);
+        return "gif-details";
+    }
+}
--- src/main/java/com/tk/model/Gif.java
+++ src/main/java/com/tk/model/Gif.java
@@ -1,17 +1,28 @@
 package com.tk.model;
+
 import java.time.LocalDate;
 
 public class Gif {
     private String name;
+    private int categoryId;
     private LocalDate dateUploaded;
     private String username;
-    private boolean favourite;
+    private boolean favorite;
 
-    public Gif(String name, LocalDate dateUploaded, String username, boolean favourite) {
+    public Gif(String name, int categoryId, LocalDate dateUploaded, String username, boolean favorite) {
         this.name = name;
+        this.categoryId = categoryId;
         this.dateUploaded = dateUploaded;
         this.username = username;
-        this.favourite = favourite;
+        this.favorite = favorite;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 
     public String getName() {
@@ -38,11 +49,11 @@ public class Gif {
         this.username = username;
     }
 
-    public boolean isFavourite() {
-        return favourite;
+    public boolean isFavorite() {
+        return favorite;
     }
 
-    public void setFavourite(boolean favourite) {
-        this.favourite = favourite;
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
     }
 }
